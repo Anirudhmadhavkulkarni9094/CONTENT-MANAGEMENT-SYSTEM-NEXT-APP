85f75fbc4cec66edb9f2bc87bb4bcc51420b47e1
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2025-04-20 11:10:55 +0530

    fix

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -31,7 +31,7 @@ export default async function BlogPage() {
 
   try {
     // Use absolute URL on the server
-    const res = await fetch("https://content-management-system-next-app.vercel.app/api/blog", {
+    const res = await fetch("http://localhost:3000/api/blog", {
       next: { revalidate: 60 }, // ISR support if needed
     });
     
